dca06fa35ed923d2f5917de2019e94f25e9349cd
huubinh49 <19120460@student.hcmus.edu.vn>
2020-09-29 20:35:54 +0700

    add requirements.txt

--- topodesignsRestAPI/settings.py
+++ topodesignsRestAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm1@0t0zz#a&rq)=zxmu%gza%#@)@r7n#*1vv!jwfxi3q!99t(3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -197,7 +197,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = 'topodesignsRestAPI/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
